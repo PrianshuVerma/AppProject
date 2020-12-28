edfbdc0f7e5cf920cc1b2f47f57fbffea131cd3e
PrianshuVerma <vermaprianshu@gmail.com>
2020-12-28 02:16:35 -0500

    idek what these files are

--- Laptop/App.js
+++ Laptop/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+        style= {{ width:150, height = 200}}
+        source={require('./assets/mac.png')}
+        />
     </View>
   );
 }
@@ -14,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1F2833',
     alignItems: 'center',
     justifyContent: 'center',
   },
