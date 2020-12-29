a02dcf25bb18188aabb3866e1b4dac99cd7a3f58
AnasKhan0607 <76663779+AnasKhan0607@users.noreply.github.com>
2020-12-29 14:40:08 -0500

    changed back

--- Laptop/screens/Search.js
+++ Laptop/screens/Search.js
@@ -8,25 +8,7 @@ const Search = ({navigation}) => {
   return (
     
     <View style={styles.container}>
-      const [country, setCountry] = useState("uk")
-      <DropDownPicker
-        
-        items={[
-          {label: 'USA', value: 'usa', icon: () => <Icon name="flag" size={18} color="#900" />, hidden: true},
-          {label: 'UK', value: 'uk', icon: () => <Icon name="flag" size={18} color="#900" />},
-          {label: 'France', value: 'france', icon: () => <Icon name="flag" size={18} color="#900" />},
-        ]}
-        defaultValue={country}
-        containerStyle={{height: 40}}
-        style={{backgroundColor: '#fafafa'}}
-        itemStyle={{
-          justifyContent: 'flex-start'
-          }}
-        dropDownStyle={{backgroundColor: '#fafafa'}}
-        onChangeItem={item => this.setState({
-          country: item.value
-        })}
-      />
+     
       <Text>This is the Search Page</Text>
       <Button
                 title="View Result"
