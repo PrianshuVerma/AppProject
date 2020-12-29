c2a3701ce9a38eb8aea945bdf521def8baac6d4c
PrianshuVerma <vermaprianshu@gmail.com>
2020-12-29 01:41:41 -0500

    fsd

--- Laptop/screens/Home.js
+++ Laptop/screens/Home.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, ImageBackground } from 'react-native';
  
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
- 
+const image = { uri: "https://wallpaperaccess.com/full/1182654.png" };
+
 const Home = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <Text>This is the Home Screen</Text>
-      <Button
-        title="Go To Search Screen"
-        onPress={() => navigation.navigate('Search Page')}
-        />
+        <ImageBackground source={image} style={styles.image}>
+            <Text style={styles.text}>This is the Home Screen</Text>
+            <Button
+                title="Go To Search Screen"
+                onPress={() => navigation.navigate('Search Page')}
+            />
+        </ImageBackground>
     </View>
   );
 };
  
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: "column"
+    },
+
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
+    },
+    text:{
+        textAlign: "center",
+        fontFamily: "Verdana-Bold",
+        fontSize: 30,
+        color: "#66FCF1"
+    }
+  });
+
 export default Home;
