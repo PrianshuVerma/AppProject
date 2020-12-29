0ec1eda8b718d367afdffa00f5af2beec0f2e235
PrianshuVerma <vermaprianshu@gmail.com>
2020-12-29 14:21:33 -0500

    fsd

--- Laptop/screens/Search.js
+++ Laptop/screens/Search.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
- 
+import DropDownPicker from 'react-native-dropdown-picker';
+import Icon from 'react-native-vector-icons/Feather';
 
 
 const Search = ({navigation}) => {
   return (
+    
     <View style={styles.container}>
+      const [country, setCountry] = useState("uk")
+      <DropDownPicker
+        
+        items={[
+          {label: 'USA', value: 'usa', icon: () => <Icon name="flag" size={18} color="#900" />, hidden: true},
+          {label: 'UK', value: 'uk', icon: () => <Icon name="flag" size={18} color="#900" />},
+          {label: 'France', value: 'france', icon: () => <Icon name="flag" size={18} color="#900" />},
+        ]}
+        defaultValue={country}
+        containerStyle={{height: 40}}
+        style={{backgroundColor: '#fafafa'}}
+        itemStyle={{
+          justifyContent: 'flex-start'
+          }}
+        dropDownStyle={{backgroundColor: '#fafafa'}}
+        onChangeItem={item => this.setState({
+          country: item.value
+        })}
+      />
       <Text>This is the Search Page</Text>
       <Button
                 title="View Result"
@@ -14,6 +35,7 @@ const Search = ({navigation}) => {
     </View>
   );
 };
+
  
 const styles = StyleSheet.create({
     container: {
