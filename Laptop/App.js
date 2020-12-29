0748dc770a188a8cd0487acfe41a354180d15eb4
PrianshuVerma <vermaprianshu@gmail.com>
2020-12-29 00:53:49 -0500

    work

--- Laptop/App.js
+++ Laptop/App.js
@@ -2,18 +2,25 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+import Search from './screens/Search';
  
- 
+const RootStack = createStackNavigator();
+
 const App = () => {
   return (
 
     <NavigationContainer>
-        {
+        
             <View style={styles.container}>
                 <Text>Hello React Native!</Text>
             </View>
-        }
-
+        
+            <RootStack.Navigator>
+                <RootStack.Screen name="Search Page" component={Search} />
+                <RootStack.Screen name="Home Page" component={Home} />
+             </RootStack.Navigator>
     </NavigationContainer>
   );
 };
