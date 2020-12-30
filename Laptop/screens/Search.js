d69e34261f75ed8c90613b33df8213ff34dd1368
PrianshuVerma <vermaprianshu@gmail.com>
2020-12-30 16:28:18 -0500

    dfsa

--- Laptop/screens/Search.js
+++ Laptop/screens/Search.js
@@ -7,22 +7,22 @@ import DropDownPicker from 'react-native-dropdown-picker';
 const Search = ({navigation}) => {
 
   // used for the first picker for the brand of the laptop
-  const [Brand, setBrand] = useState('Any');
+  const [Brand, setBrand] = useState('any');
 
   // used for the second picker for the ram of the laptop
-  const [Ram, setRam] = useState('Any');
+  const [Ram, setRam] = useState('any');
 
   // used for the third picker for the processor of the laptop
-  const [Processor, setProcessor] = useState('Any');
+  const [Processor, setProcessor] = useState('any');
   
   // used for the fourth picker for the hardrive of the laptop
-  const [Hardrive, setHardrive] = useState('Any');
+  const [Hardrive, setHardrive] = useState('any');
 
   // used for the fifth picker for the storage of the laptop
-  const [Storage, setStorage] = useState('Any');
+  const [Storage, setStorage] = useState('any');
 
   // used for the sixth picker for the price of the laptop
-  const [Price, setPrice] = useState('Any');
+  const [Price, setPrice] = useState('any');
   
 
 
@@ -37,7 +37,7 @@ const Search = ({navigation}) => {
           {label: "Any",               value: "any"},
           {label: 'Mac',              value: 'mac'},
           {label: 'Window',           value: 'window'},
-          {label: "Helward Package",  value:"hp" },
+          {label: "Hewlett-Packard",  value:"hp" },
           {label:"Asus",              value:"asus"},
           {label:"Acer",              value:"acer"},
           {label:"Microsoft",         value:"microsoft" },
@@ -54,40 +54,35 @@ const Search = ({navigation}) => {
         <View style = {styles.ramContainer}>
       <DropDownPicker
         items={[
-          {label: "All",               value: "all"},
-          {label: 'Mac',              value: 'mac'},
-          {label: 'Window',           value: 'window'},
-          {label: "Helward ",  value:"hp" },
-          {label:"Asus",              value:"asus"},
-          {label:"Acer",              value:"acer"},
-          {label:"Microsoft",         value:"microsoft" },
-          {label: "Dell",             value:"dell"},
+          {label: "Any",                value: "any"},
+          {label: '4 GB',               value: 'low'},
+          {label: '8 GB',               value: 'medium'},
+          {label: "16 GB",              value: "high" },
+          {label: "Higher than 16 GB",  value: "mega"},
 
         ]}
-        placeholder= {"Select Brand..."}
+        placeholder= {"Select Ram..."}
         defaultIndex={0}
         containerStyle={{height: 40, width: 175, alignSelf: "center"}}
-        onChangeItem={item => setBrand(item.value)}
+        onChangeItem={item => setRam(item.value)}
       />
         </View>
 
         <View style = {styles.processorContainer}>
       <DropDownPicker
         items={[
-          {label: "Any",               value: "any"},
-          {label: 'Intel Core 8th Gen',              value: 'mac'},
-          {label: 'Intel Core 7th Gen',           value: 'window'},
-          {label: "Intel Core i9",        value:"hp" },
-          {label: "Intel Core i7",              value:"asus"},
-          {label: "Intel Core i5",              value:"acer"},
-          {label: "Intel Core i3",         value:"microsoft" },
-          {label: "Unified Memory",             value:"dell"},
-          {label: 'AMD Ryzen 7',           value: 'window'},
-          {label: 'AMD Ryzen 5',           value: 'window'},
-          {label: 'AMD Ryzen 3',           value: 'window'},
-          {label: 'AMD Ryzen 7',           value: 'window'},
-          {label: "Intel Core i3",         value:"microsoft" },
-          {label: "Unified Memory",             value:"dell"},
+          {label: "Any",                  value: "any"},
+          {label: 'Intel Core 8th Gen',   value: '8th Gen'},
+          {label: 'Intel Core 7th Gen',   value: '7th Gen'},
+          {label: "Intel Core i9",        value: "i9" },
+          {label: "Intel Core i7",        value: "i7"},
+          {label: "Intel Core i5",        value: "i5"},
+          {label: "Intel Core i3",        value: "i3" },
+          {label: "M1 Chip",              value: "m1"},
+          {label: 'AMD Ryzen 7',          value: 'amd7'},
+          {label: 'AMD Ryzen 5',          value: 'amd5'},
+          {label: 'AMD Ryzen 3',          value: 'amd3'},
+          {label: "Other",                value: "other" },
         ]}
         placeholder= {"Select Processor..."}
         defaultIndex={0}
@@ -121,7 +116,7 @@ const Search = ({navigation}) => {
           {label: "2TB and Higher",          value: "mega"},
 
         ]}
-        placeholder= {"Select your Storage..."}
+        placeholder= {"Select Storage..."}
         defaultIndex={0}
         containerStyle={{height: 40, width: 175, alignSelf: "center"}}
         onChangeItem={item => setStorage(item.value)}
@@ -135,7 +130,8 @@ const Search = ({navigation}) => {
           {label: "$0.00 - $399.99",      value: "low"},
           {label: "$400.00 - $699.99",    value: 'medium'},
           {label: "$700.00 - $999.99",    value: 'high'},
-          {label: "$1000.00 and Higher",  value:"premium" },
+          {label: "$1000.00 - $1999.99",  value:"high-end" },
+          {label: "$2000.00 and Higher",  value:"premium" },
 
         ]}
         placeholder= {"Select Price..."}
