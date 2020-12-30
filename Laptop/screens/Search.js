c032e4735b52ef8187673abc3a278f81af60f955
Harshil-Patel28 <76663814+Harshil-Patel28@users.noreply.github.com>
2020-12-30 00:05:22 -0500

    picker

--- Laptop/screens/Search.js
+++ Laptop/screens/Search.js
@@ -13,7 +13,7 @@ const Search = ({navigation}) => {
     <View style={styles.container}>
       <Text>This is the Search Page</Text>
 
-
+      <View style = {styles.brandContainer}>
       <DropDownPicker
         items={[
           {label: "All",               value: "all"},
@@ -30,9 +30,91 @@ const Search = ({navigation}) => {
         defaultIndex={"fsdfsdf"}
         containerStyle={{height: 40, width: 175, alignSelf: "center"}}
         onChangeItem={item => setBrand(item.value)}
-
       />
+        </View>
 
+        <View style = {styles.ramContainer}>
+      <DropDownPicker
+        items={[
+          {label: "All",               value: "all"},
+          {label: 'Mac',              value: 'mac'},
+          {label: 'Window',           value: 'window'},
+          {label: "Helward Package",  value:"hp" },
+          {label:"Asus",              value:"asus"},
+          {label:"Acer",              value:"acer"},
+          {label:"Microsoft",         value:"microsoft" },
+          {label: "Dell",             value:"dell"},
+
+        ]}
+        placeholder= {"Select Brand..."}
+        defaultIndex={"fsdfsdf"}
+        containerStyle={{height: 40, width: 175, alignSelf: "center"}}
+        onChangeItem={item => setBrand(item.value)}
+      />
+        </View>
+
+        <View style = {styles.processorContainer}>
+      <DropDownPicker
+        items={[
+          {label: "All",               value: "all"},
+          {label: 'Mac',              value: 'mac'},
+          {label: 'Window',           value: 'window'},
+          {label: "Helward Package",  value:"hp" },
+          {label:"Asus",              value:"asus"},
+          {label:"Acer",              value:"acer"},
+          {label:"Microsoft",         value:"microsoft" },
+          {label: "Dell",             value:"dell"},
+
+        ]}
+        placeholder= {"Select Brand..."}
+        defaultIndex={"fsdfsdf"}
+        containerStyle={{height: 40, width: 175, alignSelf: "center"}}
+        onChangeItem={item => setBrand(item.value)}
+      />
+        </View>
+
+        <View style = {styles.storageContainer}>
+      <DropDownPicker
+        items={[
+          {label: "All",               value: "all"},
+          {label: 'Mac',              value: 'mac'},
+          {label: 'Window',           value: 'window'},
+          {label: "Helward Package",  value:"hp" },
+          {label:"Asus",              value:"asus"},
+          {label:"Acer",              value:"acer"},
+          {label:"Microsoft",         value:"microsoft" },
+          {label: "Dell",             value:"dell"},
+
+        ]}
+        placeholder= {"Select Brand..."}
+        defaultIndex={"fsdfsdf"}
+        containerStyle={{height: 40, width: 175, alignSelf: "center"}}
+        onChangeItem={item => setBrand(item.value)}
+      />
+        </View>
+
+        <View style = {styles.priceContainer}>
+      <DropDownPicker
+        items={[
+          {label: "All",               value: "all"},
+          {label: 'Mac',              value: 'mac'},
+          {label: 'Window',           value: 'window'},
+          {label: "Helward Package",  value:"hp" },
+          {label:"Asus",              value:"asus"},
+          {label:"Acer",              value:"acer"},
+          {label:"Microsoft",         value:"microsoft" },
+          {label: "Dell",             value:"dell"},
+
+        ]}
+        placeholder= {"Select Brand..."}
+        defaultIndex={"fsdfsdf"}
+        containerStyle={{height: 40, width: 175, alignSelf: "center"}}
+        onChangeItem={item => setBrand(item.value)}
+      />
+        </View>
+
+
+        
       <Button   
                 //style={{ marginTop: 300}}
                 title="View Result"
@@ -48,6 +130,23 @@ const styles = StyleSheet.create({
       flex: 1,
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    brandContainer: {
+      position: 'relative'
+
+    },
+    ramContainer: {
+
+    },
+    processorContainer:{
+
+
+    },
+    storageContainer: {
+
+    },
+    priceContainer: {
+
     },
   });
    
