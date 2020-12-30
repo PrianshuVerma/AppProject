70398c9980be0cb7dc7a03562cdaed2f15767388
PrianshuVerma <vermaprianshu@gmail.com>
2020-12-29 19:49:44 -0500

    fs

--- Laptop/screens/Search.js
+++ Laptop/screens/Search.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import Icon from 'react-native-vector-icons/Feather';
